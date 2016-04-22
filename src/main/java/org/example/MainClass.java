e5db9e79b39b595be99379def8d38bbd6fcf5de3
gcube@research-infrastructures.eu <gabriele.giammatteo@eng.it>
2016-04-22 22:10:05 +0200

    third commit

--- src/main/java/org/example/MainClass.java
+++ src/main/java/org/example/MainClass.java
@@ -4,4 +4,8 @@ package org.example;
  * Created by ggiammat on 4/22/16.
  */
 public class MainClass {
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
 }
